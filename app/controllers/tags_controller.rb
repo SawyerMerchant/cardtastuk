cf6a56f4b37ff68429cc3f16395fd67022719c61
John Sawyer <sawyer@sawyermerchant.com>
2017-07-28 16:52:39 -0400

    Change routes to fix admin

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,6 @@
 class TagsController < ApiController
+  def index
+    @tags = Tag.select("id, name, small_img_url, medium_img_url, orientation, size, category_id").all
+    render json: @tags.to_json
+  end
 end
