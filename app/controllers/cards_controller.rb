b37e89eed42eac98d39ae6d42bc9edd3ad1288c9
John Sawyer <sawyer@sawyermerchant.com>
2017-07-31 14:41:01 -0400

    Complete pricing

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,17 +1,19 @@
 class CardsController < ApiController
   # GET /cards
   def index
-    @cards = Card.includes(:tags).select("id, name, small_img_url, medium_img_url, orientation, size, category_id").all
+    @cards = Card.includes(:tags).select("id, name, small_img_url, medium_img_url, orientation, size, category_id, price_id").all
     render json: @cards.to_json(:include => { :category => { :only => [:name] },
-                                              :price => { :only => [:name] },
-                                              :tags => { :only => [:name] }})
+                                              :tags => { :only => [:name] },
+                                              :price => { :except => [:id, :name, :created_at, :created_at]}
+                                              })
   end
 
   # GET /cards/:id
   def show
     @card = Card.find(params[:id])
     render json: @card.to_json(:include => { :category => { :only => [:name] },
-                                             :price => { :only => [:name] },
-                                             :tags => { :only => [:name] }})
+                                             :tags => { :only => [:name] },
+                                             :price => { :except => [:id, :name, :created_at, :created_at]}
+                                             })
   end
 end
--- spec/factories/prices.rb
+++ spec/factories/prices.rb
@@ -1,4 +1,10 @@
 FactoryGirl.define do
   factory :price do
+     x25 300
+    x100 290
+    x250 281
+    x500 234
+   x1000 202
+   x2000 185
   end
 end
