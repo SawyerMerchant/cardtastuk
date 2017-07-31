9a92d1b20524d4fb72723ba5f11e6c8ecadc5242
Christian Florez <florez.christian@gmail.com>
2017-07-31 08:41:02 -0400

    add basic sass boilerplate to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Navbar, Jumbotron} from 'react-bootstrap';
+
 
 class App extends Component {
-  componentDidMount() {
-    window.fetch('api/v1/cards')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => console.log(error))
-  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Card.tastuk.com</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+<Navbar fluid>
+<Navbar.Header>
+  <Navbar.Brand>
+    <a>CardTastuk</a>
+  </Navbar.Brand>
+</Navbar.Header>
+</Navbar>
       </div>
     );
   }
