d1f0e16fdf8805f908994de8293e0de1bf371d3f
John Sawyer <sawyer@sawyermerchant.com>
2017-08-01 13:35:19 -0400

    Pause auth testing

--- spec/acceptance/auth_spec.rb
+++ spec/acceptance/auth_spec.rb
@@ -3,24 +3,49 @@ require 'rspec_api_documentation/dsl'
 require 'acceptance_helper'
 
 resource "Authorizations" do
-  # attributes = FactoryGirl.attributes_for(:user)
+  header "Accept", "application/json"
+  header "Content-Type", "application/json"
   # parameter :email, "sawyermerchant@gmail.com", required: true, scope: :user
   post "/auth" do
-    header "Content-Type", "application/json"
-    # parameter :email, "sawyermerchant@gmail.com", required: true, scope: :user
-    # parameter :password, "password", required: true, scope: :user
-    # parameter :password_confirmation, "password", required: true, scope: :user
-    params = {
-      :email => "sawyermerchant@gmail.com",#, :required => true, :scope => :params
-      :password => "password",#, :required => true, :scope => :params
-      :password_confirmation => "password",#, :required => true, :scope => :params
-      :confirm_success_url => "tastuk.com"
-    }.to_json
-    let(:raw_post) {params}
-    example "Email registration" do
-      do_request
+    # params = {
+    #   :email => "sawyermerchant@gmail.com",
+    #   :password => "password",
+    #   :password_confirmation => "password",
+    #   :confirm_success_url => "tastuk.com"
+    # }.to_json
+    # let(:raw_post) {params}
+    parameter :email, "sawyermerchant@gmail.com", :required => true#, :scope => ""
+    parameter :password, "password", :required => true#, :scope => ""
+    parameter :password_confirmation, "password", :required => true#, :scope => ""
+    parameter :confirm_success_url, "http://tastuk.com", :required => true#, :scope => ""
+
+    response_field :email, "sawyermerchant@gmail.com", :scope => :user, "Type" => "String"
+    response_field :password, "password", :scope => :user, "Type" => "String"
+    response_field :password_confirmation, :scope => :user, "Type" => "String"
+    response_field :confirm_success_url, "http://tastuk.com", :scope => :user, "Type" => "String"
+
+    let(:email) { "sawyermerchant@gmail.com" }
+    let(:password) { "password" }
+    let(:password_confirmation) { "password" }
+    let(:confirm_success_url) { "http://tastuk.com" }
+
+    let(:raw_post) {params.to_json}
+
+    example_request "Email registration" do
+      # explanation "Create an user via api"
+#
+      # user = JSON.parse(response_body)
+      # byebug
+      # expect("id", "created_at", "updated_at").to eq({
+      #   "email" => email
+        # "confirm_success_url" => confrim_success_url
+        # })
       byebug
-      expect(status).to eq(200)
+      expect(status).to eq(201)
+      client.get(URI.parse(response_headers["location"]).path, {}, headers)
+      expect(status.to eq(200))
+      # do_request
+      # expect(status).to eq(200)
       # expect {do_request}.to change{User.count}.by(1)
     end
   end
