33cc68a4d54540a82216ffac83417befa988289b
John Sawyer <sawyer@sawyermerchant.com>
2017-08-03 08:43:01 -0400

    Add lists to users rendering

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
 
+  has_many :lists
+
+  def as_json(options={})
+    super(options).merge({lists: self.lists})
+  end
+
 end
