9557a9e10c92718a4ed04b49f9df22b1661ed307
Christian Florez <florez.christian@gmail.com>
2017-08-03 20:05:35 -0400

    fix price tier calculators on client

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -16,17 +16,20 @@ export const getParams = query => {
 
 export const calculatePrice = (quantity, prices) => {
   let total;
+
   if (quantity >= 1 && quantity <= 25) {
+    total = moneyFormatter.format("USD", prices.x25 / 100 * 25);
+  } else if (quantity < 100) {
     total = moneyFormatter.format("USD", prices.x25 / 100 * quantity);
-  } else if (quantity <= 100) {
+  } else if (quantity < 250) {
     total = moneyFormatter.format("USD", prices.x100 / 100 * quantity);
-  } else if (quantity <= 250) {
+  } else if (quantity < 500) {
     total = moneyFormatter.format("USD", prices.x250 / 100 * quantity);
-  } else if (quantity <= 500) {
+  } else if (quantity < 1000) {
     total = moneyFormatter.format("USD", prices.x500 / 100 * quantity);
-  } else if (quantity <= 1000) {
+  } else if (quantity < 2000) {
     total = moneyFormatter.format("USD", prices.x1000 / 100 * quantity);
-  } else if (quantity > 1000) {
+  } else if (quantity >= 2000) {
     total = moneyFormatter.format("USD", prices.x2000 / 100 * quantity);
   } else {
     total = 0;
@@ -38,16 +41,18 @@ export const calculatePrice = (quantity, prices) => {
 const _calculatePriceUnformatted = (quantity, prices) => {
   let total;
   if (quantity >= 1 && quantity <= 25) {
+    total = prices.x25 * 25;
+  } else if (quantity < 100) {
     total = prices.x25 * quantity;
-  } else if (quantity <= 100) {
+  } else if (quantity < 250) {
     total = prices.x100 * quantity;
-  } else if (quantity <= 250) {
+  } else if (quantity < 500) {
     total = prices.x250 * quantity;
-  } else if (quantity <= 500) {
+  } else if (quantity < 1000) {
     total = prices.x500 * quantity;
-  } else if (quantity <= 1000) {
+  } else if (quantity < 2000) {
     total = prices.x1000 * quantity;
-  } else if (quantity > 1000) {
+  } else if (quantity >= 2000) {
     total = prices.x2000 * quantity;
   } else {
     total = 0;
