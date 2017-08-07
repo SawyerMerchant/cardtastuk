86732d18ece0b2abea43110c6398dd1a33ef01e8
John Sawyer <sawyer@sawyermerchant.com>
2017-08-07 14:36:22 -0400

    Admin route work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
-  # include DeviseTokenAuth::Concerns::SetUserByToken
-  # protect_from_forgery with: :null_session
-  # def fallback_index_html
-  #   render :file => 'public/index.html'
-  # end
-
-  protected
+  def fallback_index_html
+    render "public/index.html.erb"
+  end
 
 
 end
