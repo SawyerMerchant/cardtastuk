caba43b28dd86ff9ccc76de952a2e691bf5fcaa4
John Sawyer <sawyer@sawyermerchant.com>
2017-08-07 12:13:47 -0400

    Add paperclip to cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -17,6 +17,4 @@ class CardsController < ApiController
                                              })
   end
 
-  def create
-  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -3,5 +3,14 @@ class Card < ApplicationRecord
   belongs_to :price
   has_and_belongs_to_many :tags
   has_attached_file :aws_image, styles: { medium: "300x300", thumb: "100x100" }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  # validates_attachment_content_type :aws_image, content_type: /\Aimage\/.*\Z/
+  do_not_validate_attachment_file_type :aws_image
+
+  after_initialize :init
+
+    def init
+      self.price_id  ||= 1
+
+    end
+
 end
