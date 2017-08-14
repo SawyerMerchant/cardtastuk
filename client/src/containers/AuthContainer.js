32d52501fa4098225a4dcef5849b6f26258bbf5d
Christian Florez <florez.christian@gmail.com>
2017-08-13 23:19:59 -0400

    change auth redirect functionality and fix client session clearing

--- client/src/containers/AuthContainer.js
+++ client/src/containers/AuthContainer.js
@@ -6,20 +6,20 @@ import { withRouter } from "react-router-dom";
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    onLogin: e => {
+    onLogin: (e, cardRedirectId) => {
       e.preventDefault();
       const form = e.target;
       const data = serialize(form, { hash: true });
-      dispatch(loginUser(data, ownProps.history));
+      dispatch(loginUser(data, ownProps.history, cardRedirectId));
     },
-    onRegister: e => {
+    onRegister: (e, cardRedirectId) => {
       e.preventDefault();
       const form = e.target;
       const data = serialize(form, { hash: true });
       if (data.password === data.password_confirmation) {
-        dispatch(registerUser(data, ownProps.history));
+        dispatch(registerUser(data, ownProps.history, cardRedirectId));
       } else {
-        ownProps.history.push("/auth?error=bad_password");
+        ownProps.history.push("/auth?error=badPassword");
       }
     }
   };
