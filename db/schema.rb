07c1b664a09e140e8d6ad8d1238229909afb37eb
John Sawyer <sawyer@sawyermerchant.com>
2017-08-14 19:48:33 -0400

    Add referrals to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814143146) do
+ActiveRecord::Schema.define(version: 20170814233528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -224,6 +224,8 @@ ActiveRecord::Schema.define(version: 20170814143146) do
     t.json     "tokens"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
+    t.integer  "organization_id"
+    t.integer  "admin_user_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
@@ -239,4 +241,6 @@ ActiveRecord::Schema.define(version: 20170814143146) do
   add_foreign_key "orders", "users"
   add_foreign_key "proofs", "orders", column: "line_item_id"
   add_foreign_key "recipients", "lists"
+  add_foreign_key "users", "admin_users"
+  add_foreign_key "users", "organizations"
 end
