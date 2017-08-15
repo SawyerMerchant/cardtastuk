9021596435a001de5e9d651639238ffec0b7115e
John Sawyer <sawyer@sawyermerchant.com>
2017-08-14 20:02:08 -0400

    Add file: to default route method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   def fallback_index_html
     puts "%%%%%%%%%%hit fallback_index_html%%%%%%%%%%%%%"
-    # render Rails.public_path.join("index.html")
+    render file: 'public/index.html'
   end
 
-  def current_organization
-    @current_organization ||= ::Subdomains::Organization[request.subdomain]
-  end
+  # def current_organization
+  #   @current_organization ||= ::Subdomains::Organization[request.subdomain]
+  # end
 
 end
