b7aa8f1c14b5e1566541c67d020fb1da93838b02
John Sawyer <sawyer@sawyermerchant.com>
2017-08-17 15:02:58 -0400

    Add protrait and landscape proof controller actions

--- app/controllers/proofs_controller.rb
+++ app/controllers/proofs_controller.rb
@@ -13,5 +13,12 @@ class ProofsController < ApplicationController
 
   def portrait
     layout "portrait"
+    @proof = Proof.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "proof_file_name", :page_height => '10.25in', :page_width => '7.25in'  # Excluding ".pdf" extension.
+      end
+    end
   end
 end
