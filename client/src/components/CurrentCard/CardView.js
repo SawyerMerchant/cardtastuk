18989baa7221ce19407864a9642ed1bf82bd4774
Christian Florez <florez.christian@gmail.com>
2017-08-20 12:51:01 -0400

    handle first name bug

--- client/src/components/CurrentCard/CardView.js
+++ client/src/components/CurrentCard/CardView.js
@@ -9,6 +9,12 @@ const CardView = ({
   signature,
   userName
 }) => {
+  let firstName;
+  if (!currentList.first_record) {
+    firstName = "<First Name>";
+  } else {
+    firstName = currentList.first_record.first_name;
+  }
   return (
     <Col md={6} xs={12}>
       <div className="card-edit-container">
