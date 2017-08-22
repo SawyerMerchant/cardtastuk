d0a4fb142216786cb9d2ef61ddca67e35e466afa
John Sawyer <sawyer@sawyermerchant.com>
2017-08-22 08:54:11 -0400

    Remove unneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'shortened_urls/show'
 
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {registrations:  'registrations', confirmations: 'confirmations'}
 
