97792a92f26b76c0b5ef95e69c5d5f6a40f4d6cc
John Sawyer <sawyer@sawyermerchant.com>
2017-08-23 12:45:58 -0400

    Fix view helper bug

--- app/helpers/active_admin/view_helpers.rb
+++ app/helpers/active_admin/view_helpers.rb
@@ -5,6 +5,7 @@ module ActiveAdmin::ViewHelpers
       "Add a list of email addresses and names to send out personalized email invitations."
     elsif current_admin_user.role == "leader"
       "Add the names and email addresses of #{current_admin_user.organization}'s members to email out invitations to join the fundraising effort."
+    end
   end
 
 end
